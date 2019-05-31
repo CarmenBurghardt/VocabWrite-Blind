b5f52da0192432d19e41eeb1c44b0e17d221c195
Carmen <cburghardt@babbel.com>
2019-05-31 14:58:32 +0200

    basis

--- items.js
+++ items.js
@@ -1,28 +1,43 @@
+// items or word that are going to be translated by the user.
+//item: display translation 
+//article: the article translate in learning language( e.g la or le)
+//word: the noun in learning language( needs to fill in), awnser to question
+//translation: the total translation in the learning language
+//audio: the audio reference that pronounces the word(item) in the learning language
+//Used by script.js
+// Carmen Burghardt 29-05-2019
+//Adding some longer words
+
 const items = [
     {
        item: 'the fruit',
         article:'la',
         word: 'fruta',
-        translation: 'la fruta'
+        translation: 'la fruta',
+        audio: 'fruta.mp3'
     },
     { 
         item: 'the banana',
         article:'el',
         word: 'plátano',
-        translation: 'el platano'
-
+        translation: 'el platano',
+        audio: 'platano.mp3'
     },
+
+
     { 
         item: 'the apple' ,
         article:'la',
         word: 'manzana',
-        translation: 'la manzana'
+        translation: 'la manzana',
+        audio: 'manzana.mp3'
 
     },
     { item: 'the carrot',
         article:'la',
         word: 'zanahoria',
-        translation: 'la zanahoria'
+        translation: 'la zanahoria',
+        audio: 'zanahoria.mp3'
     }
 
 ];
--- script.js
+++ script.js
@@ -1,122 +1,208 @@
 
-
+//exercise to translate words
+//VARIABLES
+// counter trough items(words)
 let countItem =0;
+// id input from user in inputfield 
 let inputValue = document.getElementById("inputvalue");
+//id button to skip or to go to the next item 
 const submitButton = document.getElementById("submitButton");
-let statusAwnser= true;
+//lesson number for progress bar 
 let lessonNumber=0;
+//id from progress bar
 let progress= document.getElementById("progress");
+// points for having good or wrong awnsers
 let score=0;
+// id for score in banner
 let idScore= document.getElementById("score");
-//console.log("String"  + correctAwnser);
-console.log(typeof correctAwnser);
-//alert(typeof correctAwnser);
+//Skip button 
+let skipButton = document.getElementById('skipButton');
 
 
+// If nothing is loaded on the screen load the first tiem
 
 if(countItem == 0){
   updateItem(0);
 
 }
 
+
+//Update the item dependend which item there is
+
 function updateItem(countItem){
-  let articleNext = document.getElementById('article');
-  let itemNext = document.getElementById('item');
   
-        let nextItem= items[countItem].item;
-        let nextAritcle=  items[countItem].article;    
-        //display item 
-         document.getElementById('item').innerHTML= nextItem ;
+  //get id from article
+  let articleId = document.getElementById('article');
 
-        // display article
-        articleNext.textContent =  items[countItem].article + " ";
-         // clear screen in inputfield
-        inputValue.value = '';
-        if(countItem== items.length){
-          alert("you are done");
-          countItem=0;
-        }
-    }
+  // get id where to display the item 
+  let itemId = document.getElementById('item');
+
+  // get next noun
+  
+  let nextItem= items[countItem].item;
+  
+  // get next article
+  let nextAritcle=  items[countItem].article;    
+
+  //display item 
+  itemId .innerHTML= nextItem ;
+
+  // display article
+  articleId.textContent = nextAritcle + " ";
+        
+   // clear screen in inputfield
+  inputValue.value = '';
+      
+  //when all items are done, give feedback the user is done
+  if(countItem == items.length){
+    giveFeedback("you are done");
+    updateProgressbar();
+    countItem=0;
+  }
+}
+
+// plays sounds fitting to the fitting item
+function playSound(){
+  // read new audio from item.js
+      let newAudio = new Audio(items[countItem].audio);
+      newAudio.play(newAudio);
+}
 
 
-
+// update prog
 function updateProgressbar(){
-lessonNumber++;
-progress.innerHTML =lessonNumber;
+  // calculate what exercise is in lesson
+  lessonNumber++;
+  // display progress
+  progress.innerHTML ="Progress" + lessonNumber ;
 }
 
 
-
+// everytime awnser is right, update score with 3
 function updateScore(){
+  // calculate score
   score= score+3;
-  idScore.innerHTML= score;
+
+  // dipaly score
+  idScore.innerHTML= "Score: " + score;
 }
 
 
+// compares the awnser from the user with the correct one
 
 function compareAwnser(){
+  // correct awnser, from item.js
   let correctAwnser = items[countItem].word;
+  // amount of mistakes in awnser
   let mistake = 0;
+
+  // split the awnser and correct for comparing
   let splittedWord= correctAwnser.split('');
   let submitAwnser= inputValue.value.split('');
+
+  // which letters the user have been mistaken in
   let mistakenLetters;
-  let positionWrongLetter;
-  console.log(correctAwnser);
-  console.log(submitAwnser);
  
-  // // compare for length
+  //if letter is wrong where in the word is it written 
+  let positionWrongLetter;
+
+  //create new button for skipping,next
+  let button = document.createElement("button");
+
+  // style button 
+  button.classList.add("btn");
+  button.classList.add("btn-secondary");
+  button.classList.add("mt-5");
+     
+  // // compare for length, if length not the same awnser is wrong
   if(correctAwnser.length !== submitAwnser.length){
-    giveFeedback("the awnser is not correct, please try again" + "length");
+    giveFeedback("the awnser is not correct, please try again");
   }
 
-  //
-
+  
+// check for mistakes
   for(let i=0; i< correctAwnser.length; i++ ){
-    
+
+    // check if the letters are the same
     if(correctAwnser[i]!== submitAwnser[i]){
        mistake++;
        mistakenLetters= submitAwnser[i];
        positionWrongLetter=i;
 
-    }
-    
-   }  
+    } 
+   }  // if you make 1 mistake you have it almost correct
     if (mistake ==1 ){
       //positionWrongLetter++;
-      console.log("you were almost correct, look at the letter: "+ mistakenLetters+ "at position"+ " "+ positionWrongLetter+ "in your awnser");
-      giveFeedback("you were almost correct, look at the letter: "+ mistakenLetters+ "that is the "+ " "+ positionWrongLetter+" "+"in your awnser");
-   
+      
+      giveFeedback("You were almost correct. Please improve your awnser or press skip");
+      console.log("look at the letter: "+ mistakenLetters+" "+"that is letter "+ " "+ positionWrongLetter+" "+"in your awnser");
+      button.innerHTML = "skip";
+      // 2. Append somewhere
+     
+      skipButton.appendChild(button);
+      // 3. Add event handler
+      button.addEventListener ("click", function() {
+        skipButton.removeChild(button);
+        updateScreen();
+        });
 
-    }
-     if(mistake ==0){
-
-     giveFeedback("The anwser is correct");
-     updateScreen();
-    }
-    if(mistake >1){
-    giveFeedback("the awnser is not correct, please try again" + "mistake");
+      inputValue.addEventListener('keypress',function(e){
+        if(e.keyCode ==13){
+        e.preventDefault();
+        skipButton.removeChild(button);
         }
+      });
+     
+    }
+    // if you make no mistakes
+     if(mistake ==0){
+       document.getElementById("inputvalue").disabled = true;
 
+      giveFeedback("The awnser is correct,please press next");
+       button.innerHTML = "next";
+      // 2. Append somewhere
+     
+      skipButton.appendChild(button);
+      // 3. Add event handler
+      button.addEventListener ("click", function() {
+        skipButton.removeChild(button);
+        updateScreen();
+    
+      });
+
+    }
+
+      // awnser is also wrong when more than 1 mistake is made
+    if(mistake >1){
+
+    giveFeedback("The awnser is not correct, please try again");
+        }
 
 }
 
 
+
+
+
+//TODO: scaffolding
 function updateHelpButtons(){}
 
 
-//event listeners
-submitButton.addEventListener('click', ()=>{
-  compareAwnser();
- });
+
+//Event listener for if user submit awnser 
 
 inputValue.addEventListener('keypress',function(e){
 
-    console.log("text");
+    // if enter is pressed
    if(e.keyCode ==13){
+    // to avoid refreshing of page the whole time
     e.preventDefault();
+    // check if input is correct
         compareAwnser();
     }
 });
+
+// feedback if awnser is wrong or right 
 
 function giveFeedback(message) {
 
@@ -124,68 +210,40 @@ function giveFeedback(message) {
   const alert = document.querySelector("#alert");
   const tmpl = document.querySelector("#feedback-tmpl");
 
-  alert.innerHTML = '';
+  alert.innerHTML = '';// clear the feedback
   const clone = tmpl.content.cloneNode(true);
   clone.querySelector('.feedback').textContent = message;
   alert.appendChild(clone);
 }
 
 
-
+// update the total screen
 
 function updateScreen() {
-   giveFeedback("");
+  // enable the inputfield
+  document.getElementById("inputvalue").disabled = false;
+
+  // clear feedback
+  giveFeedback("");
+
   // update next item 
   countItem++;
-  //load next item 
-    updateItem(countItem);
+ 
+  //load next item and update score
+  updateItem(countItem);
   updateScore();
 
+  //update progressbar when for items are passed
   if (countItem == 4) {
     updateProgressbar();
   }
 }
 
-// function giveFeedback(message){
-  
-//   console.log("give feedback");
-//   const btn = document.querySelector("#submitButton");
-//   const alert = document.querySelector("#alert");
-//   const tmpl = document.querySelector("#feedback-tmpl");
-//   const clone = tmpl.content.cloneNode(true);
-//   clone.querySelector('.feedback').textContent=message;
-//   alert.appendChild(clone);
-//   // if( >1){
-//   //   console.log("too much feedback");
-
-
-//   // }
-//   let feedbackMessage = document.getElementsByClassName('feedback');
-
-//   if(feedbackMessage => 1){
-//     console.log("to much feedback" + " "+ feedbackMessage[1].textContent );
-
-//   }
-
-
-// }
 
 
 
 
-
-function updateScreen(){
-  // update next item 
-        countItem++; 
-      //load next item 
-        updateItem(countItem);
-        updateScore();
-        if(countItem ==4){
-          updateProgressbar();
-        }
-}
-
-//Save awnser
+//Save awnsers, for feedback/review
 let newAwnser = [];
 
 function saveAwnsers(){
